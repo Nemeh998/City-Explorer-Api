5af3036c05537b16a71962d1b9bbdf10885cfbf2
nemh <nemh998@gmail.com>
2021-06-19 19:48:52 +0300

    lab10

--- controller/Weather.controller.js
+++ controller/Weather.controller.js
@@ -1,24 +1,64 @@
-const Weather = require('../models/Weather.model');
 const axios = require('axios');
-const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
-//const wData = require('./data/weather.json');
+const Weather = require('../models/Weather.model');
 require('dotenv').config();
+const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
+const Cache = require('./helper/cache');//1Cache
+const { response } = require('express');
+//const wData = require('./data/weather.json');
 
+const cacheObj = new Cache();//2Cache
 const weatherController = (req, res) => {
+
+
     const lat = req.query.lat;
     const lon = req.query.lon;
-    console.log(lat);
+
+
+    const requestKey = `weather-${lat}-${lon}`;
+
+    // console.log(requestKey);
+    // console.log(cacheObj[requestKey], "cacheObj[requestKey]");
+
+    // console.log(lat);
+
+
+
+
     if (lat && lon) {
-        const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`
+        
+        if (cacheObj[requestKey]&&(Date.now()-cacheObj[requestKey].timestamp < 86400000)) {
 
-        axios.get(weatherUrl).then(response => {
-            const respData = response.data.data.map(obj => new Weather(obj));
+            console.log("==========================");
+            console.log("from the cache object");
+            console.log("==========================");
+            res.json(cacheObj[requestKey].data);
+        } 
+        else {
 
-            res.json(respData)
-        }).catch(error => {
-            res.send(error.message);
-        });
-        console.log(weatherUrl);
+
+            const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`
+
+            axios.get(weatherUrl).then(response => {
+                const respData = response.data.data.map(obj => new Weather(obj));
+
+                console.log("==========================");
+                console.log("from the cache axios request");
+                console.log("==========================");
+                console.log("==========================");
+                console.log("storing data in to our request");
+                console.log("==========================");
+                cacheObj[requestKey].data= respData;
+                //each time to request
+                cacheObj[requestKey].timestamp=Date.now();//1timestamp
+                res.json(respData);
+//time out to clear the cache 
+            }).catch(error => {
+                res.send(error.message);
+            });
+          
+        }
+
+
     }
     else {
         res.send('please provide the proper lat and lon ');
